c5ed60a07fd60656d4f7d4652f8abac4b28097d5
Gowtham N <125636305+gowthamnagarajvb@users.noreply.github.com>
2025-08-12 20:14:39 +0530

    Add files via upload

---
+++ Day-01/Day-1/TypeCasting_Operators.js
@@ -0,0 +1,47 @@
+
+//IMplicit TypeCasting //
+console.log(typeof(10+"10"));
+console.log(10+"10");
+console.log(undefined + undefined);
+console.log(NaN + NaN);
+console.log(undefined + NaN);
+console.log(1 - "1");
+console.log(100 - "50" + "Hello");
+console.log("false" - 20);
+
+
+//Explicit Typecasting //
+console.log(Number("10") + 20);
+console.log(Boolean(1) + "hello");
+console.log(String(900) - 800);
+console.log("hello" + false);
+console.log(true - true);
+console.log(1 - true);
+console.log("10" - 10);
+console.log(Number("10") + 10);
+console.log(String(10) + 10);
+console.log(Boolean(0)+false);
+console.log(String(1) + Boolean(1));
+
+
+//. ---------------------------------OPERATORS-------------------------//
+// 10 + 10 = 20 [(10,10 are operands) (+ is operator) (20 is the value/result)]
+
+// 1) Arithematic Operator  --> [ADDITION(+) SUBTRATION(-) MULTIPLICATION(*) DIVISION(/) MODULUS(%)]
+
+let a = Number(prompt("Enter the value of a: "));
+let b = Number(prompt("Enter the value of b: "));
+let add = a + b;
+let sub = a-b;
+let mul = a*b;
+let div = a/b;
+let mod = a%b;
+console.log(add); 
+console.log(sub);  
+console.log(mul);
+console.log(div);
+console.log(mod);
+
+
+
+
