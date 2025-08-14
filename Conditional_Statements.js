f6ba7fa69595932135b7fce65e479e909b722c56
Gowtham N <125636305+gowthamnagarajvb@users.noreply.github.com>
2025-08-14 11:51:00 +0530

    Add files via upload

---
+++ Conditional_Statements.js
@@ -0,0 +1,68 @@
+// Conditional Statements - Falsy values..
+// let a =[]
+// if (a){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+    
+// }
+
+
+
+// let age = Number(prompt("Enter Your age: "));
+// if (age >= 30){
+//     console.log("Uncle");
+    
+// }
+// else if (age<=18){
+//     console.log("Teen");
+    
+// }
+// else if (age<12){
+//     console.log("Child");
+    
+// }
+// else if (age>18){
+//     console.log("Adult");
+    
+// }
+
+
+// ------------------------Switch case------------------------//
+
+// let week = prompt("Enter the weekdays").toLocaleLowerCase();
+// switch(week){
+//     case "monday" : console.log(("Hey Welcome its the page its Monday"));
+//     break
+//     case "tuesday" : console.log(("Hey Welcome its the page its Tuesday"));
+//     break
+//     case "wednesday" : console.log(("Hey Welcome its the page its Wednesday"));
+//     break
+//     case "thursday" : console.log(("Hey Welcome its the page its Thursday"));
+//     break
+//     case "friday" : console.log(("Hey Welcome its the page its Friday"));
+//     break
+//     case "saturday" : console.log(("Hey Welcome its the page its Saturday"));
+//     break
+//     case "sunday" : console.log(("Hey Welcome its the page its Sunday"));
+//     break
+// }
+
+//--------------------------------Loops-------------------------//
+
+ //----------------------WHile----DO WHile------For Loop----------------------//
+ //1) While
+let a =1;
+while(a<=10){
+    console.log();
+    a++
+}
+ 
+//2) Do-While
+let i = 0;
+do {
+  console.log("The number is: " + i);
+  i++;
+} while (i <= 10);
+
